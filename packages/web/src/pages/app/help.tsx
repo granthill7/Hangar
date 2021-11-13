d89e716757dd4edf388cf2371f1f31076461f6c5
John Kahn <7807353+johnkahn@users.noreply.github.com>
2021-11-13 08:58:21 -0600

    WIP

--- packages/web/src/pages/app/help.tsx
+++ packages/web/src/pages/app/help.tsx
@@ -9,33 +9,47 @@ const HelpPage: NextPage = () => (
     <Box backgroundColor={useColorModeValue('gray.200', 'gray.900')}>
       <AppContainer>
         <HStack paddingY={12}>
-          <VStack alignItems="stretch" maxWidth="30em">
-            <Heading>Get Help</Heading>
+          <VStack flex={1} alignItems="stretch" spacing={8}>
+            <Heading>Technical Help</Heading>
             <Text>
               We&apos;re here to help you succeed this weekend! We&apos;ve got some great developers
-              here and can help with a ton of technologies.
+              here and can help with a ton of technologies, even if you aren&apos;t hacking on our
+              challenge.
             </Text>
             <HStack>
               <Button>Join Technical Help Queue</Button>
+            </HStack>
+          </VStack>
+          <VStack flex={1} alignItems="stretch" spacing={8}>
+            <Heading>Idea Pitch Help</Heading>
+            <Text>
+              Wanting to hack on our challenge? The airline business is pretty complex, so please
+              come pitch your idea to us! We&apos;re here to help and will give as much feedback you
+              need on your idea.
+            </Text>
+            <HStack>
               <Button>Join Idea Pitch Queue</Button>
             </HStack>
           </VStack>
-          <Text></Text>
         </HStack>
       </AppContainer>
     </Box>
-    <HStack>
-      <VStack>
-        <Heading as="h2" size="md">
-          Technical Help Queue
-        </Heading>
-      </VStack>
-      <VStack>
-        <Heading as="h2" size="md">
-          Idea Pitch Queue
-        </Heading>
-      </VStack>
-    </HStack>
+    <Box paddingTop={16}>
+      <AppContainer>
+        <HStack>
+          <VStack>
+            <Heading as="h2" size="md">
+              Technical Help Queue
+            </Heading>
+          </VStack>
+          <VStack>
+            <Heading as="h2" size="md">
+              Idea Pitch Queue
+            </Heading>
+          </VStack>
+        </HStack>
+      </AppContainer>
+    </Box>
   </AppLayout>
 );
 
